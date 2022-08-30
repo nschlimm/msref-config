c2bded6aef823345a5a5468b38354e83c333873e
Niklas Schlimm <ns103@hotmail.de>
2022-08-30 19:10:18 +0200

    Initial

---
+++ src/test/java/com/nschlimm/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nschlimm.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
